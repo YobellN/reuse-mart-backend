98108d0d81ba9c3de1b8f5f104ad269f6bcb7811
Dina Oktavia Pangaribuan <140843745+sg-riley@users.noreply.github.com>
2025-05-12 20:39:48 +0700

    update model donasi

--- app/Models/Donasi.php
+++ app/Models/Donasi.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Donasi extends Model
 {
-    //
+    protected $table = 'donasi';
+    protected $primaryKey = 'id_donasi';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_donasi',
+        'id_request_donasi',
+        'id_produk',
+        'tanggal_donasi',
+        'nama_penerima',
+        'total_poin',
+    ];
+
+    public function requestDonasi()
+    {
+        return $this->belongsTo(RequestDonasi::class, 'id_request_donasi');
+    }
+
+    public function produk()
+    {
+        return $this->belongsTo(Produk::class, 'id_produk');
+    }
+
+
 }
